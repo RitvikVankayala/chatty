3602fe10d9554b2b8e9d4439753668f5563946a0
Ritvik Vankayala <v.n.b.p.ritvik@gmail.com>
2024-05-19 23:33:50 +0530

    first commit

---
+++ client/src/utils/apirouts.js
@@ -0,0 +1,7 @@
+export const host = "https://chat-app-iam-beside-you-iit-dhanbad.onrender.com";
+export const regRoute = `${host}/api/auth/reg`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userroutes");
+const messageRoute = require("./routes/messagesRoute");
+const socket = require("socket.io");
+const app = express();
+require("dotenv").config();
+
+const path = require("path");
+
+const morgan = require("morgan");
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    // useNewParser:true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(path.resolve(__dirname, "client"), "dist")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(path.resolve(__dirname, "client"), "dist"));
+  });
+}
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Started on port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    Credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
+});
